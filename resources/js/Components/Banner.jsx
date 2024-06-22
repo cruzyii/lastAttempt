4224eb60dece350e3fdabbfa48193548c4561297
cruzyii <cruzycraft@gmail.com>
2024-06-22 05:37:48 +0300

    feat: Update Banner component with new styling and text

--- resources/js/Components/Banner.jsx
+++ resources/js/Components/Banner.jsx
@@ -5,6 +5,10 @@ function Banner() {
     return (
         <>
             <img src={BannerImg} alt='Image of a girl'/>
+            <div className='p-8 font-bold text-center font-AbhayaLibre'>
+                <h1 className='text-7xl'>STYLE REMAINS</h1>
+                <h2 className='text-3xl'>Fashion fades, only style remains the same</h2>
+            </div>
         </>
     );
 }
