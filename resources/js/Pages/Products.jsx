dc34d60b96aebe7d6547ec90b4a27d51a0a7852b
cruzyii <cruzycraft@gmail.com>
2024-06-25 20:47:03 +0300

    refactor: Update Products page layout and styling

--- resources/js/Pages/Products.jsx
+++ resources/js/Pages/Products.jsx
@@ -1,16 +1,28 @@
 import Layout from "@/Components/Layout";
 import Subnav from "@/Components/Subnav";
 import Sidebar from "@/Components/Sidebar";
+import { IoIosMenu } from "react-icons/io";
+import { FaChevronDown } from "react-icons/fa";
 
 function Products(){
     return (
             <Layout>
                 <Subnav/>
-                <div className="flex flex-1 px-[72px] py-4 font-medium text-xl justify-between">
+                <div className="flex flex-1 px-[72px] py-4 font-medium text-l justify-between">
                     <h1 className="text-2xl">Vīriešu Apģērbi(3)</h1>
-                    <ul className="flex space-x-16">
-                        <li>Filtri</li>
-                        <li>Kārtot pēc</li>
+                    <ul className="flex space-x-4">
+                        <li>
+                            <div className="flex items-center">
+                                <p className="px-1">Slēpt Filtrus</p>
+                                <IoIosMenu className='opacity-50 cursor-pointer hover:opacity-100 ml-auto' size={25}/>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="flex items-center">
+                                <p className="px-1">Kārtot pēc</p>
+                                <FaChevronDown className='opacity-50 cursor-pointer hover:opacity-100 ml-auto' size={15}/>
+                            </div>
+                        </li>
                     </ul>
                 </div>
                 <Sidebar/>
