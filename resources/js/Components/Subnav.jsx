37aaf22a6e7040ab81f90ceca77be90812c87da2
cruzyii <cruzycraft@gmail.com>
2024-06-25 18:56:51 +0300

    refactor: Update Subnav component layout and styling

--- resources/js/Components/Subnav.jsx
+++ resources/js/Components/Subnav.jsx
@@ -2,11 +2,11 @@ import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 function Subnav(){
     return (
-        <div className="bg-gray-100 flex">
-            <div className="flex items-center py-4 m-auto">
-                <MdChevronLeft/>
-                <h2>Free shipping on orders €50+</h2>
-                <MdChevronRight/>
+        <div className="bg-gray-100 flex justify-center">
+            <div className="flex items-center justify-between py-3 w-[600px]">
+                <MdChevronLeft className='opacity-50 cursor-pointer hover:opacity-100' size={30}/>
+                <h2 className="underline font-bold text-sm">Free shipping on orders €50+</h2>
+                <MdChevronRight className='opacity-50 cursor-pointer hover:opacity-100' size={30}/>
             </div>
         </div>
     )
